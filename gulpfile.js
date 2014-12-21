0354add398393d67da10342ae175133e4882cb4f
Brent Jackson <jacksonblack@gmail.com>
2014-12-20 23:00:00 -0500

    Styles

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+
+var gulp = require('gulp');
+var swig = require('gulp-swig');
+var path = require('path');
+var api = require('jxnblk-api');
+
+gulp.task('swig', function() {
+  var options = {
+    setup: function(swig) {
+      swig.setDefaults({
+        loader: swig.loaders.fs(path.resolve(__dirname, '../docs/src/templates'))
+      });
+    },
+    defaults: {
+      cache: false
+    },
+    data: api
+  };
+  gulp.src('./templates/**/*.html')
+    .pipe(swig(options))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', ['swig'], function() {
+  gulp.watch(['./templates/**/*'], ['swig']);
+});
+
+gulp.task('default', ['watch']);
+
