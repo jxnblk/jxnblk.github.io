7b598b789c48c86dc1a02a1c564b77589ce7a30e
Brent Jackson <jxnblk@github.com>
2015-03-27 17:18:06 -0400

    Use static-react v2 beta

--- scripts/build.js
+++ scripts/build.js
@@ -1,36 +1,27 @@
 
 var _ = require('lodash');
+var fs = require('fs');
 var path = require('path');
-var staticReact = require('static-react');
+var build = require('static-react').build;
 var Root = require('../src/root.jsx');
 
 var props = require('jxnblk-api');
 
-props.routes = [
-  { path: '/', title: 'Home' }
-];
-
-if (process.env.NODE_ENV === 'production') {
-  console.log('production build');
-  props.baseUrl = '';
-} else {
-  console.log('development build');
-  props.baseUrl = '';
-}
-
 props.title = 'Jxnblk';
 props.description = 'Brent Jackson';
 props.stylesheet = 'http://d2v52k3cl9vedd.cloudfront.net/blk/1.0.1/blk.min.css';
-//props.script = '/js/app.min.js'
 
 props.headerLinks = [
   { href: '//twitter.com/jxnblk', title: 'Twitter' },
   { href: '//github.com/jxnblk', title: 'Github' },
 ];
 
-staticReact({
-  dir: path.join(__dirname, '..'),
+var html = build({
   Root: Root,
-  props: props
+  props: props,
+  noScript: true
 });
+
+fs.writeFileSync(path.join(__dirname, '../index.html'), html);
+console.log('Static index.html built');
 
