399052f4943149a8cc2b3c9d931d3c0c5a32d266
jxnblk <jacksonblack@gmail.com>
2015-06-23 14:20:20 -0400

    Add link

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,13 @@
 
 module.exports = {
   other: [
+    { name: 'Work', link: '//jxnblk.com/work', description: 'Professional Work' },
     { name: 'Principles', link: '//jxnblk.com/principles', description: 'Web Design Principles' },
     { name: 'Reading List', link: '//jxnblk.com/reading-list', description: 'Recommended Reading' },
     { name: 'Ashley', link: '//jxnblk.com/Ashley', description: 'Readable Tumblr Theme' },
     { name: 'Heather', link: '//jxnblk.com/Heather', description: 'Hyperminimal Jekyll Theme' },
     { name: 'Twipster', link: '//jxnblk.com/Twipster', description: 'Simpler, Readabler Twitter' },
     { name: 'SoundRad', link: '//soundrad.com', description: 'Simple, Fast Soundcloud Player' },
-    //{ name: 'MrsJxn', link: '//mrsjxn.com', description: 'Post-Future Beats' },
     { name: 'Microbeats', link: '//microbeats.cc', description: 'Beats Created in Under an Hour' },
     { name: 'Skullcat', link: '//jxnblk.com/skullcat', description: 'A Avatar fur Mrmrs' },
     { name: 'Gifolio', link: '//jxnblk.com/gifolio', description: 'GIF Portfolio' },
