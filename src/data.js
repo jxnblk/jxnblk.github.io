0bfe89c2d3a5ba2cc6f058f08059522f64c17a71
jxnblk <jacksonblack@gmail.com>
2016-09-12 21:44:31 -0400

    Edit data

--- src/data.js
+++ src/data.js
@@ -15,17 +15,233 @@ module.exports = {
     { href: '//twitter.com/jxnblk', title: 'Twitter' },
     { href: '//github.com/jxnblk', title: 'Github' },
   ],
-  headerLinks: [
-    { href: '//jxnblk.com/writing', title: 'Writing' },
-    { href: '//twitter.com/jxnblk', title: 'Twitter' },
-    { href: '//github.com/jxnblk', title: 'Github' },
-  ],
   social_networks: [
     { name: 'Twitter', link: '//twitter.com/jxnblk' },
-    { name: 'Github', link: '//github.com/jxnblk' },
-    // { name: 'Dribbble', link: '//dribbble.com/jxnblk' },
-    // { name: 'Tumblr', link: '//jxnblk.tumblr.com' },
-    // { name: 'Soundcloud', link: '//soundcloud.com/jxnblk' }
+    { name: 'Github', link: '//github.com/jxnblk' }
+  ],
+  writing: [
+  ],
+  projects: [
+    {
+      category: 'React',
+      links: [
+        {
+          href: 'http://jxnblk.com/rebass',
+          name: 'Rebass',
+          description: 'Configurable React UI components'
+        },
+        {
+          href: 'http://jxnblk.com/reflexbox',
+          name: 'Reflexbox',
+          description: 'React flexbox layout and grid system'
+        },
+        {
+          href: 'http://jxnblk.com/reline',
+          name: 'Reline',
+          description: 'React SVG line icon components'
+        },
+        {
+          href: 'http://jxnblk.com/rgx',
+          name: 'Rgx',
+          description: 'Constraint-based grid system based on minimum and maximum widths'
+        },
+        {
+          href: 'http://jxnblk.com/gx',
+          name: 'Gx',
+          description: 'Minimal responsive React grid system based on the Fab Four Technique'
+        },
+        {
+          href: 'https://github.com/jxnblk/static-react',
+          name: 'Static React',
+          description: 'Zero-configuration CLI React static renderer'
+        },
+        {
+          href: 'http://jxnblk.com/react-geomicons',
+          name: 'React Geomicons',
+          description: 'Icon component for Geomicons Open'
+        },
+        {
+          href: 'http://jxnblk.com/react-fitter-happier-text',
+          name: 'React Fitter Happier Text',
+          description: 'React component for fully fluid headings'
+        },
+        {
+          href: 'http://jxnblk.com/react-icons',
+          name: 'React Icons',
+          description: 'How to build generative SVG icons with React'
+        },
+        {
+          href: 'https://github.com/jxnblk/react-css-grid',
+          name: 'React CSS Grid',
+          description: 'Responsive CSS-based React grid component'
+        },
+      ]
+    },
+    {
+      category: 'CSS',
+      links: [
+        {
+          href: 'http://basscss.com',
+          name: 'Basscss',
+          description: 'Low-level CSS toolkit'
+        },
+        {
+          href: 'http://cssstats.com',
+          name: 'CSS Stats',
+          description: 'CSS static-analysis tool'
+        },
+        {
+          href: 'http://jxnblk.com/gravitons',
+          name: 'Gravitons',
+          description: 'Virtually massless CSS layout microlibrary'
+        },
+        {
+          href: 'http://jxnblk.com/cxs',
+          name: 'Cxs',
+          description: 'A CSS-in-JS solution for functional CSS in functional UI components'
+        },
+        {
+          href: 'http://jxnblk.com/vhs/',
+          name: 'VHS',
+          description: 'Post-future CSS animations'
+        },
+      ]
+    },
+    {
+      category: 'Color',
+      links: [
+        {
+          href: 'http://jxnblk.com/hello-color',
+          name: 'Hello Color',
+          description: 'Functional color palette generator'
+        },
+        {
+          href: 'http://jxnblk.com/colorable',
+          name: 'Colorable',
+          description: 'Color combination contrast tester'
+        },
+        {
+          href: 'http://jxnblk.com/shade/',
+          name: 'Shade',
+          description: 'Mathematically derived gradient explorer'
+        },
+        {
+          href: 'http://jxnblk.com/Spectral/',
+          name: 'Spectral',
+          description: 'Click the rainbow'
+        },
+      ]
+    },
+    {
+      category: 'Design Tools',
+      links: [
+        {
+          href: 'http://jxnblk.com/formula/',
+          name: 'Formula',
+          description: 'CSS calculator for bulletproof form and button styles that always line up'
+        },
+        {
+          href: 'http://jxnblk.com/modular/',
+          name: 'Modular',
+          description: 'Don’t compose without a scale'
+        },
+        {
+          href: 'http://jxnblk.com/paths/',
+          name: 'Paths',
+          description: 'Build and edit SVGs in the browser'
+        },
+        {
+          href: 'http://geomicons.com',
+          name: 'Geomicons Open',
+          description: 'Open source SVG icons for the web'
+        },
+        {
+          href: 'http://jxnblk.com/loading/',
+          name: 'Loading...',
+          description: 'This could take a while'
+        },
+        {
+          href: 'http://jxnblk.com/papercraft/',
+          name: 'Papercraft',
+          description: 'Hand-coded SVG lettering'
+        },
+      ]
+    },
+    {
+      category: 'Music',
+      links: [
+        {
+          href: 'http://jxnblk.com/skullcat',
+          name: 'Skullcat',
+          description: 'A avatar fur mrmrs'
+        },
+        {
+          href: 'http://jxnblk.com/stepkit',
+          name: 'Stepkit',
+          description: 'Web audio step sequencer'
+        },
+        {
+          href: 'http://microbeats.cc',
+          name: 'Microbeats',
+          description: 'Beats created in under an hour'
+        },
+      ]
+    },
+    {
+      category: 'Themes',
+      links: [
+        {
+          href: 'https://github.com/jxnblk/hypercolors',
+          name: 'Hypercolors',
+          description: 'Generative hyperterm color theme'
+        },
+        {
+          href: 'https://github.com/jxnblk/hyperterminator',
+          name: 'hyperterminator',
+          description: 'Hyperterm theme for cybernetic organisms'
+        },
+        {
+          href: 'http://jxnblk.com/Twipster/',
+          name: 'Twipster',
+          description: 'Simpler, Readabler Twitter'
+        },
+        {
+          href: 'http://jxnblk.com/Heather/',
+          name: 'Heather',
+          description: 'Hyperminimal Jekyll theme'
+        },
+      ]
+    },
+    {
+      category: 'Miscellaneous',
+      links: [
+        {
+          href: 'http://jxnblk.com/principles',
+          name: 'Principles',
+          description: 'Principles of web design'
+        },
+        {
+          href: 'https://jxnblk.com/hyp',
+          name: 'hyp',
+          description: 'Hyperminimal functional DOM element microlibrary'
+        },
+        {
+          href: 'https://github.com/jxnblk/h0',
+          name: 'h0',
+          description: 'Hyperminimal functional DOM element microlibrary'
+        },
+        {
+          href: 'http://jxnblk.com/gifolio',
+          name: 'Gifolio',
+          description: 'GIF portfolio'
+        },
+        {
+          href: 'http://jxnblk.com/reading-list/',
+          name: 'Reading List',
+          description: 'Recommended reading on web design and development'
+        },
+      ]
+    }
   ],
   scripts: {
     ga: '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create", "UA-4603832-6", "auto");ga("send", "pageview");'
